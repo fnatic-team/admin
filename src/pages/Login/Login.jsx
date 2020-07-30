218606ec029ab713b019024617fb48645ef6208c
JuanBiantong <jbiantong@gmail.com>
2020-07-30 08:59:21 +0700

    add login form

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { FormLogin } from "../../components";
+export default function Login() {
+    return (
+        <div>
+            <FormLogin />
+        </div>
+    );
+}
