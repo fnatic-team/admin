55fa2876ce4dd372b320daa8efe461b60eb6c69c
JuanBiantong <jbiantong@gmail.com>
2020-08-03 18:10:33 +0700

    get data

--- src/pages/Admins/Admins.jsx
+++ src/pages/Admins/Admins.jsx
@@ -23,7 +23,6 @@ const useStyles = makeStyles((theme) => ({
     },
     table: {
         padding: theme.spacing(3),
-        // paddingRight: theme.spacing(3),
     },
     tablehead: {
         padding: '0',
