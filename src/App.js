027e6cc3b68334ccdce4be6363a43e3c9071e008
JuanBiantong <jbiantong@gmail.com>
2020-08-03 19:33:35 +0700

    add private route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { MainMenu } from "./components";
+// import { MainMenu } from "./components";
 import { Login } from "./pages";
 // import store from "./redux/store";
 // import { Provider } from "react-redux";
@@ -16,9 +16,9 @@ function App() {
                     <Route exact path="/">
                         <Login />
                     </Route>
-                    <Route path="/dashboard">
+                    {/* <PrivateRoute path="/dashboard">
                         <MainMenu />
-                    </Route>
+                    </PrivateRoute> */}
                 </Switch>
             </Router>
         // </Provider>
---
+++ src/redux/actions/login.js
@@ -0,0 +1,60 @@
+import Swal from "sweetalert2";
+export const LOGIN = "LOGIN";
+const url = process.env.REACT_APP_API_URL;
+
+export const login = (values, history) => async () => {
+    try {
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(values),
+        };
+
+        const response = await fetch(`${url}/api/admin/login`, options);
+        const result = await response.json();
+
+        if (response.status === 200) {
+            localStorage.setItem("token", result.result);
+
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                onOpen: (toast) => {
+                    toast.addEventListener("mouseenter", Swal.stopTimer);
+                    toast.addEventListener("mouseleave", Swal.resumeTimer);
+                },
+            });
+
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully",
+            });
+
+            setTimeout(() => {
+                history.push("/dashboard");
+            }, 3000);
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Forbidden",
+                text: result.message,
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const logout = (history) => async () => {
+    Swal.fire({
+        icon: "success",
+        title: "Terimakasih",
+    });
+    localStorage.removeItem("token");
+    history.push("/");
+};
