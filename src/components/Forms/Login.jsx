7cc8daffee79b260391d93e6a5a3deedd97cac84
Rohmad <rohmad37@ymail.com>
2020-08-07 22:08:52 +0700

    update styling

--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Desktop from '../assets/desktop.png'
-import Logo1 from '../assets/logo_black_full.png'
-import CardMedia from '@material-ui/core/CardMedia';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Desktop from "../assets/desktop.png";
+import Logo1 from "../assets/new_logo.png";
+import CardMedia from "@material-ui/core/CardMedia";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/actions";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -18,42 +18,42 @@ import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: '5% auto',
-    width: '50%',
-    display: 'flex',
-    backgroundColor: '#3a6986',
-    MozBorderRadius: '10px',
-    WebkitBorderRadius: '10px',
-    borderRadius: '10px',
+    margin: "5% auto",
+    width: "50%",
+    display: "flex",
+    backgroundColor: "#3a6986",
+    MozBorderRadius: "10px",
+    WebkitBorderRadius: "10px",
+    borderRadius: "10px",
   },
   head: {
-    width: '40%',
-    margin: 'auto',
-    display: 'flex',
+    width: "40%",
+    margin: "auto",
+    display: "flex",
     marginBottom: theme.spacing(2),
     marginTop: theme.spacing(2),
   },
   image: {
-    backgroundRepeat: 'no-repeat',
-    backgroundColor: 'lightblue',
-    backgroundPosition: 'center',
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "lightblue",
+    backgroundPosition: "center",
     paddingLeft: theme.spacing(5),
     paddingRight: theme.spacing(5),
   },
 
   media: {
     height: 0,
-    paddingTop: '100%', // 16:9
+    paddingTop: "100%", // 16:9
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: '#6cc7d9',
+    backgroundColor: "#6cc7d9",
   },
   form: {
     marginTop: theme.spacing(1),
@@ -65,14 +65,14 @@ const useStyles = makeStyles((theme) => ({
 
 const CustomField = (props) => {
   return (
-      <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          size="larger"
-          {...props}
-      />
+    <TextField
+      fullWidth
+      variant="outlined"
+      margin="normal"
+      required
+      size="larger"
+      {...props}
+    />
   );
 };
 
@@ -83,17 +83,42 @@ export default function Login() {
 
   return (
     <Grid container component="main" className={classes.root}>
-       <CssBaseline />
-       <Grid item xs={12} sm={12} md={12} lg={12} elevation={6}>
+      <CssBaseline />
+      <Grid item xs={12} sm={12} md={12} lg={12} elevation={6}>
         <img src={Logo1} alt="" className={classes.head} />
       </Grid>
-      <Grid item xs={12} sm={12} md={7} lg={7} className={classes.image} elevation={6} square>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={7}
+        lg={7}
+        className={classes.image}
+        elevation={6}
+        square
+      >
         <CardMedia
-          item xs={12} sm={12} md={7} lg={7} className={classes.media} elevation={6} square
+          item
+          xs={12}
+          sm={12}
+          md={7}
+          lg={7}
+          className={classes.media}
+          elevation={6}
+          square
           image={Desktop}
         />
       </Grid>
-      <Grid item xs={12} sm={12} md={5} lg={5} component={Paper} elevation={6} square>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={5}
+        lg={5}
+        component={Paper}
+        elevation={6}
+        square
+      >
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
@@ -104,56 +129,56 @@ export default function Login() {
           <Formik
             initialValues={{ username: "", password: "" }}
             validate={(values) => {
-                const errors = {};
-                if (!values.username) {
-                    errors.username = "Required";
-                } 
-                if (!values.password) {
-                    errors.password = "Required";
-                }
-                return errors;
+              const errors = {};
+              if (!values.username) {
+                errors.username = "Required";
+              }
+              if (!values.password) {
+                errors.password = "Required";
+              }
+              return errors;
             }}
             onSubmit={(values) => {
-                dispatch(login(values, history));
+              dispatch(login(values, history));
             }}
-        >
+          >
             {() => (
-                <Form className={classes.form}>
-                    <Field
-                        type="username"
-                        as={CustomField}
-                        name="username"
-                        label="Username"
-                        autoFocus
-                    />
-                    <ErrorMessage
-                        name="username"
-                        component="div"
-                        className={classes.error}
-                    />
-                    <Field
-                        type="password"
-                        as={CustomField}
-                        name="password"
-                        label="Password"
-                    />
-                    <ErrorMessage
-                        name="password"
-                        component="div"
-                        className={classes.error}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign In
-                    </Button>
-                </Form>
+              <Form className={classes.form}>
+                <Field
+                  type="username"
+                  as={CustomField}
+                  name="username"
+                  label="Username"
+                  autoFocus
+                />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className={classes.error}
+                />
+                <Field
+                  type="password"
+                  as={CustomField}
+                  name="password"
+                  label="Password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className={classes.error}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Sign In
+                </Button>
+              </Form>
             )}
-        </Formik>
+          </Formik>
         </div>
       </Grid>
     </Grid>
--- src/pages/Speaker/components/PendingSpeaker.jsx
+++ src/pages/Speaker/components/PendingSpeaker.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect } from "react";
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Paper from '@material-ui/core/Paper';
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import RejectedIcon from "@material-ui/icons/Clear";
 import ApprovedIcon from "@material-ui/icons/CheckBox";
 import { getPendingSpeaker, updateStatusSpeaker } from "../../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
-import { green } from '@material-ui/core/colors';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
-import moment from 'moment';
-
+import { green } from "@material-ui/core/colors";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import moment from "moment";
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -31,7 +30,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -47,12 +46,22 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-  { id: 'cv', numeric: true, disablePadding: false, label: 'Documentation' },
-  { id: 'category', numeric: true, disablePadding: false, label: 'Expertation' },
-  { id: 'createdAt', numeric: true, disablePadding: false, label: 'Registered At' },
-  { id: 'status', numeric: true, disablePadding: false, label: 'Status' },
-  { id: '_id', numeric: true, disablePadding: false, label: 'Action' },
+  { id: "name", numeric: false, disablePadding: true, label: "Name" },
+  { id: "cv", numeric: true, disablePadding: false, label: "Documentation" },
+  {
+    id: "category",
+    numeric: true,
+    disablePadding: false,
+    label: "Expertation",
+  },
+  {
+    id: "createdAt",
+    numeric: true,
+    disablePadding: false,
+    label: "Date Registered",
+  },
+  { id: "status", numeric: true, disablePadding: false, label: "Status" },
+  { id: "_id", numeric: true, disablePadding: false, label: "Action" },
 ];
 
 function EnhancedTableHead(props) {
@@ -64,26 +73,23 @@ function EnhancedTableHead(props) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell >
-          No
-        </TableCell>
+        <TableCell>No</TableCell>
         {headCells.map((headCell) => (
-
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "default"}
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
               active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
+              direction={orderBy === headCell.id ? order : "asc"}
               onClick={createSortHandler(headCell.id)}
             >
               {headCell.label}
               {orderBy === headCell.id ? (
                 <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
                 </span>
               ) : null}
             </TableSortLabel>
@@ -98,21 +104,17 @@ EnhancedTableHead.propTypes = {
   classes: PropTypes.object.isRequired,
   onRequestSort: PropTypes.func.isRequired,
   onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
 
-
-
-
-
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
   },
   paper: {
-    width: '100%',
+    width: "100%",
     marginBottom: theme.spacing(2),
   },
   table: {
@@ -120,38 +122,38 @@ const useStyles = makeStyles((theme) => ({
   },
   visuallyHidden: {
     border: 0,
-    clip: 'rect(0 0 0 0)',
+    clip: "rect(0 0 0 0)",
     height: 1,
     margin: -1,
-    overflow: 'hidden',
+    overflow: "hidden",
     padding: 0,
-    position: 'absolute',
+    position: "absolute",
     top: 20,
     width: 1,
   },
   button: {
     margin: theme.spacing(1),
-},
+  },
 }));
 
 export default function EnhancedTable() {
   const classes = useStyles();
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('createdAt');
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("createdAt");
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-    const dispatch = useDispatch();
-    const pendingSpeakers = useSelector((state) => state.speaker.pendingSpeaker);
-    console.log(pendingSpeakers)
+  const dispatch = useDispatch();
+  const pendingSpeakers = useSelector((state) => state.speaker.pendingSpeaker);
+  console.log(pendingSpeakers);
 
-    useEffect(() => {
-        dispatch(getPendingSpeaker());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getPendingSpeaker());
+  }, [dispatch]);
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -185,68 +187,55 @@ export default function EnhancedTable() {
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
                   return (
-                    
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row._id}
-                    >
-                     
-                      <TableCell >
-                        {index+1}
-                      </TableCell>
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row._id}>
+                      <TableCell>{index + 1}</TableCell>
                       <TableCell component="th" scope="row" padding="none">
                         {row.name}
                       </TableCell>
                       <TableCell align="right">
-                        <Button variant="contained" style={{ color: green[500] }} href={row.cv} target="_blank">
-                            <AttachFileIcon />
+                        <Button
+                          variant="contained"
+                          style={{ color: green[500] }}
+                          href={row.cv}
+                          target="_blank"
+                        >
+                          <AttachFileIcon />
                         </Button>
                       </TableCell>
                       <TableCell align="right">{row.category}</TableCell>
-                      <TableCell align="right">{moment(row.createdAt).format('LL')}</TableCell>
+                      <TableCell align="right">
+                        {moment(row.createdAt).format("LL")}
+                      </TableCell>
                       <TableCell align="right">{row.status}</TableCell>
                       <TableCell align="right">
-                          <Button
-                              variant="contained"
-                              color="primary"
-                              className={classes.button}
-                              size="small"
-                              startIcon={<ApprovedIcon />}
-                              onClick={() =>
-                                  dispatch(
-                                      updateStatusSpeaker(
-                                          row._id,
-                                          "ACTIVE"
-                                      )
-                                  )
-                              }
-                          >
-                              Approve
-                          </Button>
-                          <Button
-                              variant="contained"
-                              color="secondary"
-                              className={classes.button}
-                              size="small"
-                              startIcon={<RejectedIcon />}
-                              onClick={() =>
-                                  dispatch(
-                                      updateStatusSpeaker(
-                                          row._id,
-                                          "REJECTED"
-                                      )
-                                  )
-                              }
-                          >
-                              Reject
-                          </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          className={classes.button}
+                          size="small"
+                          startIcon={<ApprovedIcon />}
+                          onClick={() =>
+                            dispatch(updateStatusSpeaker(row._id, "ACTIVE"))
+                          }
+                        >
+                          Approve
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          className={classes.button}
+                          size="small"
+                          startIcon={<RejectedIcon />}
+                          onClick={() =>
+                            dispatch(updateStatusSpeaker(row._id, "REJECTED"))
+                          }
+                        >
+                          Reject
+                        </Button>
                       </TableCell>
                     </TableRow>
                   );
                 })}
-             
             </TableBody>
           </Table>
         </TableContainer>
@@ -260,7 +249,6 @@ export default function EnhancedTable() {
           onChangeRowsPerPage={handleChangeRowsPerPage}
         />
       </Paper>
-     
     </div>
   );
 }
